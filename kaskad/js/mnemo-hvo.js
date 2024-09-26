6eb97ce0df40e8e6f5459d403b2462f48638afc3
kaskad-saim <saim.asutp@gmail.com>
2024-09-26 11:24:44 +0300

    tooltips hvo1

--- kaskad/js/mnemo-hvo.js
+++ kaskad/js/mnemo-hvo.js
@@ -88,3 +88,23 @@ if ((levelE12, valueE12Current, levelE12Percent)) {
     levelObj(0, 1600, valueE12Current, 70, levelE12, levelE12Percent, 0, 100);
   }
 }
+
+//включение/отключение всплывающих подсказок
+// tooltips
+const hoverNoneBtn = document.querySelector('.hover-none-btn');
+const hoverElemParam = document.querySelectorAll('.mnemo__tooltip');
+
+const toggleBtnText = () => {
+  hoverNoneBtn.innerHTML =
+    hoverNoneBtn.innerHTML == 'Выключить всплывающие подсказки'
+      ? 'Включить всплывающие подсказки'
+      : 'Выключить всплывающие подсказки';
+};
+
+hoverNoneBtn.addEventListener('click', () => {
+  for (let i = 0; i < hoverElemParam.length; i++) {
+    const item = hoverElemParam[i];
+    item.classList.toggle('enabled-hover');
+  }
+  toggleBtnText();
+});
